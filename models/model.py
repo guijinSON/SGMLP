4370f060dbd7e3a93fe0054f04f7639a9001feac
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2021-09-07 11:28:33 +0900

    Update model.py

--- models/model.py
+++ models/model.py
@@ -1,16 +1,27 @@
 import torch 
 import torch.nn as nn 
 import torch.nn.functional as F
+import csv
 from models.embedding import TransformerEmbedding
 from models.layer import gMLPBLOCK_CLS
 
 
+
 class gMLP(nn.Module):
-    def __init__(self,d_model,d_ffn,seq_len,num_layers):
+    def __init__(self,d_model,d_ffn,seq_len,num_layers,evaluate):
         super(gMLP,self).__init__()
-        self.model = nn.Sequential(*[gMLPBLOCK_CLS(d_model,d_ffn,seq_len) for _ in range(num_layers)])
-        
+        self.evaluate = evaluate
+        if self.evaluate:
+            route = open('weights/routing.csv','w', newline='\n')
+            wr = csv.writer(route)
+            self.model = nn.Sequential(*[gMLPBLOCK_CLS(d_model,d_ffn,seq_len,evaluate,wr) for _ in range(num_layers)])
+        else:
+            self.model = nn.Sequential(*[gMLPBLOCK_CLS(d_model,d_ffn,seq_len,evaluate) for _ in range(num_layers)])
     def forward(self,x):
+        if self.evaluate:
+            x,csv = model(x)
+            csv.close()
+            return x,csv
         x = self.model(x)
         return x
 
@@ -27,8 +38,8 @@ class MaskedLanguageModelingHead(nn.Module):
         return mlm_prediction
 
 class gMLP_LanguageModel(gMLP):
-    def __init__(self,vocab_size, d_model, d_ffn, seq_len, num_layers,output_logits=False):
-        super().__init__(d_model,d_ffn,seq_len,num_layers)
+    def __init__(self,vocab_size, d_model, d_ffn, seq_len, num_layers,output_logits,evaluate):
+        super().__init__(d_model,d_ffn,seq_len,num_layers,evaluate)
         self.embed = TransformerEmbedding(vocab_size,d_model,seq_len,0.1)
         self.output_logits = output_logits
         self.to_logits = MaskedLanguageModelingHead(vocab_size,d_model)
@@ -43,8 +54,10 @@ class gMLP_LanguageModel(gMLP):
         return output
     
 
-def build_model(num_tokens, d_model, d_ffn, seq_len, num_layers,output_logits=False):
+def build_model(num_tokens, d_model, d_ffn, seq_len, num_layers,output_logits=False,evaluate=False):
     
-    model = gMLP_LanguageModel(num_tokens,d_model,d_ffn,seq_len,num_layers,output_logits)
-
+    model = gMLP_LanguageModel(num_tokens,d_model,d_ffn,seq_len,num_layers,output_logits,evaluate)
+    #if torch.cuda.is_available():
+    #    model = model.cuda()
     return model
+
