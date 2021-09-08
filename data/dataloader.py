5030c0d9acb1d2d427fd1a2159f7669973621c13
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2021-09-09 01:03:10 +0900

    Update dataloader.py

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,23 +1,29 @@
 from datasets import load_dataset
 from data.tokenizer import load_tokenizer
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader,DistributedSampler
+
 import numpy as np
 import random
 from configs import parser
 
 args = parser.parse_args()
 
-def load_dataloader(max_length=128,batch_size=64):
-    dataset = load_dataset('c4','en', split='train', streaming=True)
-    train_dataset = dataset.skip(1000)
-    val_dataset = dataset.take(1000)
+def load_dataloader(max_length=128,batch_size=64,rank=None,world_size=None):
+    train_dataset = load_dataset("allenai/c4",'en', split='train', streaming=True)#,cache_dir='/raid/nlp/data')#, streaming=True)
+    val_dataset = load_dataset("allenai/c4",'en', split='validation', streaming=True)#,cache_dir='/raid/nlp/data')#, streaming=True)
+
+    train_dataset = train_dataset.take(256*40000)
+    val_dataset = val_dataset.take(1000)
     
     tokenizer = load_tokenizer()
     train_dataset = tokenize_dataset(train_dataset,tokenizer,max_length)
     val_dataset   = tokenize_dataset(val_dataset,tokenizer,max_length)
     
-    train_loader = DataLoader(train_dataset, batch_size=batch_size)
-    val_loader   = DataLoader(val_dataset, batch_size=batch_size)
+    #train_sampler = DistributedSampler(train_dataset,num_replicas=world_size,rank=rank,shuffle=True)
+    #val_sampler = DistributedSampler(val_dataset,num_replicas=world_size,rank=rank,shuffle=True)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size)#, sampler = train_sampler, shuffle=False, pin_memory=True)
+    val_loader   = DataLoader(val_dataset, batch_size=batch_size)#, sampler = val_sampler, shuffle=False, pin_memory=True)
     return train_loader,val_loader
 
 def tokenize_dataset(dataset,tokenizer,max_length):
