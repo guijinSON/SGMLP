d8e501bba726dca08e5d61542f647aad7e9227b1
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2021-09-09 00:58:52 +0900

    Update layer.py

--- models/layer.py
+++ models/layer.py
@@ -6,42 +6,96 @@ import torch.nn.functional as F
 class gMLPBLOCK_CLS(nn.Module):
     def __init__(self,d_model,d_ffn,seq_len,evaluate,wr=None):
         super(gMLPBLOCK_CLS,self).__init__()
-
         self.layer_norm = nn.LayerNorm(d_model)
         self.channel_proj_i = nn.Sequential(nn.Linear(d_model,d_ffn),nn.GELU())
         self.channel_proj_ii = nn.Sequential(nn.Linear(d_ffn,d_model),nn.GELU())
         self.sgu = SpatialGatingUnit_CLS(d_ffn,seq_len,evaluate,wr)
 
     def forward(self,x):
-        residual = x
+        residual = x.clone()
         x = self.layer_norm(x)
         x = self.channel_proj_i(x)
         x = self.sgu(x)
         x = self.channel_proj_ii(x)
         return residual + x
 
-class SpatialGatingUnit_CLS(nn.Module):
+class SpatialGatingUnit_Softmax(nn.Module):
     def __init__(self,d_ffn,seq_len,evaluate,wr):
-        super(SpatialGatingUnit_CLS,self).__init__()
+        super(SpatialGatingUnit_Softmax,self).__init__()
         self.layer_norm = nn.LayerNorm(d_ffn)
-        self.spatial_cls = nn.Linear(d_ffn,1)
+        self.spatial_cls = nn.Sequential(nn.Linear(d_ffn,4),nn.Softmax(dim=-1))
         self.spatial_proj_i = nn.Conv1d(seq_len,seq_len,1)
         self.spatial_proj_ii = nn.Conv1d(seq_len,seq_len,1)
-        nn.init.constant_(self.spatial_proj_i.bias, -1.0)
-        nn.init.constant_(self.spatial_proj_ii.bias, 1.0)
-        self.gelu = nn.GELU()
-        self.evaluate = evaluate
-        self.wr = wr
+        self.spatial_proj_iii = nn.Conv1d(seq_len,seq_len,1)
+        self.spatial_proj_iv = nn.Conv1d(seq_len,seq_len,1)
 
+        nn.init.constant_(self.spatial_proj_i.bias, -1.0)
+        nn.init.constant_(self.spatial_proj_ii.bias, -0.5)
+        nn.init.constant_(self.spatial_proj_iii.bias,  0.5)
+        nn.init.constant_(self.spatial_proj_iv.bias, 1.0)
+
+        self.gelu = nn.GELU()
+        self.wr = wr
+        self.evaluate = evaluate
+        
     def forward(self,x):
         residual = x.clone() #학습이 느려질 여지가 될 수도 residual / 파라미터 증가 
         x = self.layer_norm(x)
         cls  = x[:,0]
-        cls  = torch.tanh(self.spatial_cls(cls))
-        cls_idx = [0 if _<0 else 1 for _ in cls ] 
+        cls  = self.spatial_cls(cls)
+        cls_idx = torch.argmax(cls,dim=-1)
         if self.evaluate:
             self.wr.writerow(cls_idx)
-        output = [self.spatial_proj_i(torch.unsqueeze(x[n],0)) if idx==0 else self.spatial_proj_ii(torch.unsqueeze(x[n],0)) for n,idx in enumerate(cls_idx)]
-        output = torch.squeeze(torch.stack(output)) + residual
-        output = self.gelu(output)
+
+       #output = [ if idx==0 else self.spatial_proj_ii(torch.unsqueeze(x[n],0)) for n,idx in enumerate(cls_idx)]
+        output = [self.spatial_proj_i(torch.unsqueeze(x[n],0)) if n== 0 else self.spatial_proj_ii(torch.unsqueeze(x[n],0)) \
+                if n==1 else self.spatial_proj_iii(torch.unsqueeze(x[n],0)) if n==2 else self.spatial_proj_iv(torch.unsqueeze(x[n],0)) for n,idx in enumerate(cls_idx)]
+        output = torch.squeeze(torch.stack(output)) 
+        output = self.gelu(output) + residual
         return output
+
+class SpatialGatingUnit_Sigmoid(nn.Module):
+    def __init__(self,d_ffn,seq_len,evaluate,wr):
+        super(SpatialGatingUnit_Sigmoid,self).__init__()
+        self.layer_norm = nn.LayerNorm(d_ffn)
+        self.spatial_cls = nn.Sequential(nn.Linear(d_ffn,1),nn.Sigmoid())
+        self.spatial_proj_i = nn.Conv1d(seq_len,seq_len,1)
+        self.spatial_proj_ii = nn.Conv1d(seq_len,seq_len,1)
+
+        nn.init.constant_(self.spatial_proj_i.bias, -1.0)
+        nn.init.constant_(self.spatial_proj_ii.bias, -1.0)
+        self.gelu = nn.GELU()
+        self.wr = wr
+        self.evaluate = evaluate
+        
+    def forward(self,x):
+        residual = x.clone() #학습이 느려질 여지가 될 수도 residual / 파라미터 증가 
+        x = self.layer_norm(x)
+        cls  = x[:,0]
+        cls  = self.spatial_cls(cls)
+        cls_idx = torch.round(cls).type(torch.LongTensor)
+        if self.evaluate:
+            self.wr.writerow(cls_idx)
+
+        output = [self.spatial_proj_i(torch.unsqueeze(x[n],0)) if idx==0 else self.spatial_proj_ii(torch.unsqueeze(x[n],0)) for n,idx in enumerate(cls_idx)]
+        #output = [self.spatial_proj_i(torch.unsqueeze(x[n],0)) if n== 0 else self.spatial_proj_ii(torch.unsqueeze(x[n],0)) \
+        #        if n==1 else self.spatial_proj_iii(torch.unsqueeze(x[n],0)) if n==2 else self.spatial_proj_iv(torch.unsqueeze(x[n],0)) for n,idx in enumerate(cls_idx)]
+        output = torch.squeeze(torch.stack(output)) 
+        output = self.gelu(output+residual) 
+        return output
+
+class gMLPBLOCK(nn.Module):
+    def __init__(self,d_model,d_ffn,seq_len,evaluate,wr=None):
+        super(gMLPBLOCK,self).__init__()
+        self.layer_norm = nn.LayerNorm(d_model)
+        self.channel_proj_i = nn.Sequential(nn.Linear(d_model,d_ffn),nn.GELU())
+        self.channel_proj_ii = nn.Sequential(nn.Linear(d_ffn,d_model),nn.GELU())
+        self.sgu = SpatialGatingUnit_Sigmoid(d_ffn,seq_len,evaluate,wr)
+
+    def forward(self,x):
+        residual = x.clone()
+        x = self.layer_norm(x)
+        x = self.channel_proj_i(x)
+        x = self.sgu(x)
+        x = self.channel_proj_ii(x)
+        return x + residual
