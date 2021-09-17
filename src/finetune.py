d7e006a54974f4e95c38901bb461500c9157f6c7
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2021-09-17 12:07:43 +0900

    Update finetune.py

--- src/finetune.py
+++ src/finetune.py
@@ -62,7 +62,7 @@ class FinetuneTrainer():
                   validation_loss += loss.item()
                   self.metric.add_batch(predictions=pred,references=label)
 
-              wandb.log({"Train Loss": (running_loss/len(self.evaluate)),'metric':self.metric.compute(), "Validation Loss":(validation_loss/len(self.val_dataloader))})
+              wandb.log({"Train Loss": (running_loss/self.evaluate),'metric':self.metric.compute(), "Validation Loss":(validation_loss/len(self.val_dataloader))})
          # self.model.train()
         running_loss = 0.0
       torch.save(self.model.state_dict(),f'{self.task}_{epoch}.pth')
