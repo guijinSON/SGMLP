a5adbb8b0b6831b564abaa24b8512b7c21c3b263
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2021-09-17 14:12:56 +0900

    Update model.py

--- models/model.py
+++ models/model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import csv
 from models.embedding import TransformerEmbedding
-from models.layer import gMLPBLOCK
+from models.layer import gMLPBLOCK,multi_gMLPBLOCK
 
 
 
@@ -37,7 +37,41 @@ class MaskedLanguageModelingHead(nn.Module):
         
         return mlm_prediction
 
+class multi_gMLP(nn.Module):
+    def __init__(self,d_model,d_ffn,seq_len,num_layers,evaluate):
+        super(multi_gMLP,self).__init__()
+        self.evaluate = evaluate
+        if self.evaluate:
+            route = open('weights/routing.csv','w', newline='')
+            wr = csv.writer(route)
+            self.model = nn.Sequential(*[gMLPBLOCK(d_model,d_ffn,seq_len,evaluate,wr) for _ in range(num_layers)])
+        else:
+            self.model = nn.Sequential(*[multi_gMLPBLOCK(d_model,d_ffn,seq_len,evaluate,_) for _ in range(num_layers)])
+    def forward(self,x):
+        if self.evaluate:
+            x,csv = model(x)
+            csv.close()
+            return x,csv
+        x = self.model(x)
+        return x
+        
 class gMLP_LanguageModel(gMLP):
+    def __init__(self,vocab_size, d_model, d_ffn, seq_len, num_layers,output_logits,evaluate):
+        super().__init__(d_model,d_ffn,seq_len,num_layers,evaluate)
+        self.embed = TransformerEmbedding(vocab_size,d_model,seq_len,0.1)
+        self.output_logits = output_logits
+        self.to_logits = MaskedLanguageModelingHead(vocab_size,d_model)
+
+    def forward(self,x):
+        embedding = self.embed(x)
+        embedding = embedding
+        output = self.model(embedding)
+        if self.output_logits:
+            output = self.to_logits(output)
+
+        return output
+
+class gMLP_multi_LanguageModel(gMLP):
     def __init__(self,vocab_size, d_model, d_ffn, seq_len, num_layers,output_logits,evaluate):
         super().__init__(d_model,d_ffn,seq_len,num_layers,evaluate)
         self.embed = TransformerEmbedding(vocab_size,d_model,seq_len,0.1)
@@ -62,37 +96,9 @@ def build_model(num_tokens, d_model, d_ffn, seq_len, num_layers,output_logits=Fa
     return model
 
 
-class OneSentClassificationHead(nn.Module):
-  def __init__(self,vocab_size,d_model,d_ffn, maxlen,layers,weight_path,device):
-    super(OneSentClassificationHead,self).__init__()
-    self.vocab_size = vocab_size
-    self.d_model = d_model
-    self.d_ffn = d_ffn
-    self.layers = layers
-    self.maxlen = maxlen
-    self.weight_path = weight_path
-    self.device = device
+def build_multi_model(num_tokens, d_model, d_ffn, seq_len, num_layers,output_logits=False,evaluate=False):
     
-    self.model = self.load_model()
-    
-    self.pooler = nn.Sequential(nn.Linear(self.d_model,self.d_model),
-                                nn.Tanh())
-    self.projection = nn.Sequential(nn.Linear(self.d_model,5),
-                                    nn.ELU(),
-                                    nn.Linear(5,1),
-                                   nn.Sigmoid())
-    
-  def forward(self,x):
-    x = self.model(x)[:,0]
-    x = self.pooler(x)
-    x = self.projection(x)
-    return x
-    
-  def load_model(self):
-    model = build_model(self.vocab_size,self.d_model,self.d_ffn,self.maxlen,self.layers,output_logits=False)
-    weight = torch.load(self.weight_path,map_location=torch.device(self.device))
-    model_weight = {}
-    for key,val in weight.items():
-      model_weight[key[7:]] = val
-    model.load_state_dict(model_weight)
-    return model 
+    model = gMLP_multi_LanguageModel(num_tokens,d_model,d_ffn,seq_len,num_layers,output_logits,evaluate)
+    #if torch.cuda.is_available():
+    #    model = model.cuda()
+    return model
