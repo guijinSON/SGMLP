93af2174437208201b2398a1379eb79718974667
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2021-09-25 19:50:23 +0900

    Update model.py

--- models/model.py
+++ models/model.py
@@ -3,27 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import csv
 from models.embedding import TransformerEmbedding
-from models.layer import gMLPBLOCK,multi_gMLPBLOCK,gMLPBLOCK_Extended
+from models.layer import gMLPBLOCK,multi_gMLPBLOCK,gMLPBLOCK_Extended,multi_gMLPBLOCK_Extended
 
-
-
-class gMLP(nn.Module):
-    def __init__(self,d_model,d_ffn,seq_len,num_layers,evaluate):
-        super(gMLP,self).__init__()
-        self.evaluate = evaluate
-        if self.evaluate:
-            route = open('weights/routing.csv','w', newline='')
-            wr = csv.writer(route)
-            self.model = nn.Sequential(*[gMLPBLOCK(d_model,d_ffn,seq_len,evaluate,wr) for _ in range(num_layers)])
-        else:
-            self.model = nn.Sequential(*[gMLPBLOCK(d_model,d_ffn,seq_len,evaluate) for _ in range(num_layers)])
-    def forward(self,x):
-        if self.evaluate:
-            x,csv = model(x)
-            csv.close()
-            return x,csv
-        x = self.model(x)
-        return x
 
 class MaskedLanguageModelingHead(nn.Module):
     def __init__(self, vocab_size, model_dim):
@@ -37,16 +18,16 @@ class MaskedLanguageModelingHead(nn.Module):
         
         return mlm_prediction
 
-class multi_gMLP(nn.Module):
+class gMLP(nn.Module):
     def __init__(self,d_model,d_ffn,seq_len,num_layers,evaluate):
-        super(multi_gMLP,self).__init__()
+        super(gMLP,self).__init__()
         self.evaluate = evaluate
         if self.evaluate:
             route = open('weights/routing.csv','w', newline='')
             wr = csv.writer(route)
-            self.model = nn.Sequential(*[multi_gMLPBLOCK(d_model,d_ffn,seq_len,evaluate,wr) for _ in range(num_layers)])
+            self.model = nn.Sequential(*[gMLPBLOCK(d_model,d_ffn,seq_len,evaluate,wr) for _ in range(num_layers)])
         else:
-            self.model = nn.Sequential(*[multi_gMLPBLOCK(d_model,d_ffn,seq_len,evaluate,_) for _ in range(num_layers)])
+            self.model = nn.Sequential(*[gMLPBLOCK(d_model,d_ffn,seq_len,evaluate) for _ in range(num_layers)])
     def forward(self,x):
         if self.evaluate:
             x,csv = model(x)
@@ -72,6 +53,42 @@ class gMLP_Extended(nn.Module):
             return x,csv
         x = self.model(x)
         return x
+
+class multi_gMLP(nn.Module):
+    def __init__(self,d_model,d_ffn,seq_len,num_layers,evaluate):
+        super(multi_gMLP,self).__init__()
+        self.evaluate = evaluate
+        if self.evaluate:
+            route = open('weights/routing.csv','w', newline='')
+            wr = csv.writer(route)
+            self.model = nn.Sequential(*[multi_gMLPBLOCK(d_model,d_ffn,seq_len,evaluate,wr) for _ in range(num_layers)])
+        else:
+            self.model = nn.Sequential(*[multi_gMLPBLOCK(d_model,d_ffn,seq_len,evaluate,_) for _ in range(num_layers)])
+    def forward(self,x):
+        if self.evaluate:
+            x,csv = model(x)
+            csv.close()
+            return x,csv
+        x = self.model(x)
+        return x
+    
+class multi_gMLP_Extended(nn.Module):
+    def __init__(self,d_model,d_ffn,seq_len,num_layers,evaluate):
+        super(multi_gMLP_Extended,self).__init__()
+        self.evaluate = evaluate
+        if self.evaluate:
+            route = open('weights/routing.csv','w', newline='')
+            wr = csv.writer(route)
+            self.model = nn.Sequential(*[multi_gMLPBLOCK_Extended(d_model,d_ffn,seq_len,evaluate,wr) for _ in range(num_layers)])
+        else:
+            self.model = nn.Sequential(*[multi_gMLPBLOCK_Extended(d_model,d_ffn,seq_len,evaluate,_) for _ in range(num_layers)])
+    def forward(self,x):
+        if self.evaluate:
+            x,csv = model(x)
+            csv.close()
+            return x,csv
+        x = self.model(x)
+        return x
         
 class gMLP_LanguageModel(gMLP):
     def __init__(self,vocab_size, d_model, d_ffn, seq_len, num_layers,output_logits,evaluate):
@@ -89,22 +106,6 @@ class gMLP_LanguageModel(gMLP):
 
         return output
 
-class gMLP_multi_LanguageModel(multi_gMLP):
-    def __init__(self,vocab_size, d_model, d_ffn, seq_len, num_layers,output_logits,evaluate):
-        super().__init__(d_model,d_ffn,seq_len,num_layers,evaluate)
-        self.embed = TransformerEmbedding(vocab_size,d_model,seq_len,0.1)
-        self.output_logits = output_logits
-        self.to_logits = MaskedLanguageModelingHead(vocab_size,d_model)
-
-    def forward(self,x):
-        embedding = self.embed(x)
-        embedding = embedding
-        output = self.model(embedding)
-        if self.output_logits:
-            output = self.to_logits(output)
-
-        return output
-    
 class gMLP_LanguageModel_Extended(gMLP_Extended):
     def __init__(self,vocab_size, d_model, d_ffn, seq_len, num_layers,output_logits,evaluate):
         super().__init__(d_model,d_ffn,seq_len,num_layers,evaluate)
@@ -121,6 +122,40 @@ class gMLP_LanguageModel_Extended(gMLP_Extended):
 
         return output
     
+class gMLP_multi_LanguageModel(multi_gMLP):
+    def __init__(self,vocab_size, d_model, d_ffn, seq_len, num_layers,output_logits,evaluate):
+        super().__init__(d_model,d_ffn,seq_len,num_layers,evaluate)
+        self.embed = TransformerEmbedding(vocab_size,d_model,seq_len,0.1)
+        self.output_logits = output_logits
+        self.to_logits = MaskedLanguageModelingHead(vocab_size,d_model)
+
+    def forward(self,x):
+        embedding = self.embed(x)
+        embedding = embedding
+        output = self.model(embedding)
+        if self.output_logits:
+            output = self.to_logits(output)
+
+        return output
+    
+class gMLP_multi_LanguageModel_Extended(multi_gMLP_Extended):
+    def __init__(self,vocab_size, d_model, d_ffn, seq_len, num_layers,output_logits,evaluate):
+        super().__init__(d_model,d_ffn,seq_len,num_layers,evaluate)
+        self.embed = TransformerEmbedding(vocab_size,d_model,seq_len,0.1)
+        self.output_logits = output_logits
+        self.to_logits = MaskedLanguageModelingHead(vocab_size,d_model)
+
+    def forward(self,x):
+        embedding = self.embed(x)
+        embedding = embedding
+        output = self.model(embedding)
+        if self.output_logits:
+            output = self.to_logits(output)
+
+        return output
+    
+
+    
 
 def build_base_model(num_tokens=50265, d_model=768, d_ffn=2048, seq_len=128, num_layers=12,output_logits=False,evaluate=False,extended_version=False):
     if extended_version:
@@ -132,9 +167,12 @@ def build_base_model(num_tokens=50265, d_model=768, d_ffn=2048, seq_len=128, num
     return model
 
 
-def build_large_model(num_tokens=50265, d_model=768, d_ffn=2400, seq_len=128, num_layers=24,output_logits=False,evaluate=False):
-    
-    model = gMLP_multi_LanguageModel(num_tokens,d_model,d_ffn,seq_len,num_layers,output_logits,evaluate)
+def build_large_model(num_tokens=50265, d_model=768, d_ffn=2400, seq_len=128, num_layers=24,output_logits=False,evaluate=False,extended_version=False):
+    if extended_version:
+        model = gMLP_multi_LanguageModel_Extended(num_tokens,d_model,d_ffn,seq_len,num_layers,output_logits,evaluate)
+    else:   
+        model = gMLP_multi_LanguageModel(num_tokens,d_model,d_ffn,seq_len,num_layers,output_logits,evaluate)
     #if torch.cuda.is_available():
     #    model = model.cuda()
     return model
+    
